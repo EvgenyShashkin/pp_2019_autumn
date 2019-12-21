46dc684c12e98743277bb1a656c61fb47bf9c166
EvgenyShashkin <evg.shashkin@gmail.com>
2019-12-21 10:01:39 +0300

    fix task2

--- modules/task_2/shashkin_e_gather/gather.cpp
+++ modules/task_2/shashkin_e_gather/gather.cpp
@@ -9,13 +9,12 @@
 int Gather(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount,
   MPI_Datatype recvtype, int root, MPI_Comm comm) {
   int size, rank;
-  int sendtype_size, recvtype_size;
+  int type_size;
   MPI_Status status;
 
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Type_size(sendtype, &sendtype_size);
-  MPI_Type_size(recvtype, &recvtype_size);
+  MPI_Type_size(sendtype, &type_size);
 
   if (root < 0)
     throw std::runtime_error("Wrong root!\n");
@@ -23,12 +22,12 @@ int Gather(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recv
     throw std::runtime_error("Sendcount is wrong!\n");
 
   if (rank == root) {
-    memcpy(reinterpret_cast<char*>(recvbuf) + recvcount * recvtype_size * root, sendbuf,
-      sendcount * sendtype_size);
-    for (int i = 0; i < size; ++i) {
-      if (i != root)
-        MPI_Recv(reinterpret_cast<char*>(recvbuf) + recvcount * recvtype_size * i, recvcount,
-            recvtype, i, 0, comm, &status);
+    memcpy(reinterpret_cast<char*>(recvbuf) + recvcount * type_size * root, sendbuf,
+      sendcount * type_size);
+    for (int proc = 0; proc < size; ++proc) {
+      if (proc != root)
+        MPI_Recv(reinterpret_cast<char*>(recvbuf) + recvcount * type_size * proc, recvcount,
+            recvtype, proc, 0, comm, &status);
     }
   } else {
     MPI_Send(sendbuf, sendcount, sendtype, root, 0, comm);
